f20633742e58f0fe561c19fd12ea21a9c98a7bb0
Oliver <on28@njit.edu>
2018-11-02 17:23:21 -0400

    Feature: Test CarStringTest Created (Not working)

---
+++ tests/Unit/CarStringTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Tests\Unit;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class CarModelTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+
+        $user = factory(\App\Car::all(
+        $this->Make['id' => '10'], ['Make' => 'Honda']));
+    }
+}
+
