6caf6b92fc4b4725740018c149e71e918b4a93bf
Oliver <on28@njit.edu>
2018-11-02 17:26:28 -0400

    Feature: Test contactsTest Created

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -3,7 +3,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-class ContactTest extends TestCase
+class ContactsTest extends TestCase
 {
     /**
      * A basic test example.
